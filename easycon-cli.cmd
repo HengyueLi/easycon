731498aa9bdc042e1d1d84f5821a9ca4592775f9
H. Li <30506>
2022-02-25 09:48:54 +0800

    unknown header
    有一些改变，不知道是那里的

--- easycon-cli.cmd
+++ easycon-cli.cmd
@@ -1,8 +1,8 @@
 ::# do not print everything on screen ::
-@echo off  
+@echo off
 ::# project path ::
 set dir=%~dp0
 ::# python script ::
-set pyscpt=%dir%easycon
-::#run script by python in env. Args are passed::  
+set pyscpt=%dir%easycon-cli
+::#run script by python in env. Args are passed::
 python %pyscpt% %*
--- easycon.cmd
+++ easycon.cmd
@@ -3,6 +3,6 @@
 ::# project path ::
 set dir=%~dp0
 ::# python script ::
-set pyscpt=%dir%easycon-cli
+set pyscpt=%dir%easycon
 ::#run script by python in env. Args are passed::
 python %pyscpt% %*
